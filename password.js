4acfa92bc1f2aaec46da17b03edb387f11d1373c
Ariel Cardoso <arielvix@gmail.com>
2020-05-28 16:39:39 -0700

    Update html and js

--- password.js
+++ password.js
@@ -1,10 +1,13 @@
 let checkPwd = function(){
-    let password = document.getElementById("pass").value;
-    
-    if(password != "1234"){
-        alert("inccorect password!");
-        document.getElementById("pass").value="";
-        document.getElementById("pass").focus();
+    let correctPwd = "covid19";
+
+    let password = document.getElementById("pass");
+    console.log(password);
+
+    if (password.value != correctPwd){
+        alert("Incorrect password!");
+        password.value = "";
+        password.focus();
     }else{
         document.location.replace('https://www.google.ca');
     }
